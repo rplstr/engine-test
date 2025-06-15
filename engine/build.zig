0d38a0f98433788584ebd299a22282b886a8eb1d
xor-bits <oneleh@utu.fi>
2025-06-15 03:32:25 +0300

    feat(engine): wayland windowing backend

--- engine/build.zig
+++ engine/build.zig
@@ -40,16 +40,31 @@ pub fn module(
         .linux => {
             lib.linkSystemLibrary("X11");
 
+            lib.linkSystemLibrary("wayland-client");
+
             const wayland_xml = waylandXmlPath(b);
+            const wayland_protocols = waylandProtocolsPath(b);
+            const xdg_shell_xml = b.pathJoin(&.{
+                wayland_protocols,
+                "stable/xdg-shell/xdg-shell.xml",
+            });
+
+            const wayland_h = generateClientHeader(b, wayland_xml, "wayland.h");
+            const wayland_c = generateClientSource(b, wayland_xml, "wayland.c");
+            const xdg_shell_h = generateClientHeader(b, xdg_shell_xml, "xdg_shell.h");
+            const xdg_shell_c = generateClientSource(b, xdg_shell_xml, "xdg_shell.c");
+
             const wayland_includes = b.addNamedWriteFiles("wayland-includes");
-            _ = wayland_includes.addCopyFile(
-                generateClientHeader(b, wayland_xml),
-                "wayland.h",
-            );
+            _ = wayland_includes.addCopyFile(wayland_h, "wayland.h");
+            _ = wayland_includes.addCopyFile(xdg_shell_h, "xdg-shell.h");
 
             lib.addIncludePath(wayland_includes.getDirectory());
             lib.addCSourceFile(.{
-                .file = generateClientSource(b, wayland_xml),
+                .file = wayland_c,
+                .language = .c,
+            });
+            lib.addCSourceFile(.{
+                .file = xdg_shell_c,
                 .language = .c,
             });
         },
@@ -82,12 +97,40 @@ fn waylandXmlPath(b: *std.Build) []const u8 {
     });
 }
 
-fn generateClientHeader(b: *std.Build, protocol_xml_path: []const u8) std.Build.LazyPath {
-    return invokeWaylandScanner(b, "client-header", protocol_xml_path, "wayland.h");
+fn waylandProtocolsPath(b: *std.Build) []const u8 {
+    const pkg_data_dir = b.run(&.{
+        "pkg-config",
+        "--variable=pkgdatadir",
+        "wayland-protocols",
+    });
+
+    return std.mem.trim(u8, pkg_data_dir, std.ascii.whitespace[0..]);
 }
 
-fn generateClientSource(b: *std.Build, protocol_xml_path: []const u8) std.Build.LazyPath {
-    return invokeWaylandScanner(b, "private-code", protocol_xml_path, "wayland.c");
+fn generateClientHeader(
+    b: *std.Build,
+    protocol_xml_path: []const u8,
+    output_basename: []const u8,
+) std.Build.LazyPath {
+    return invokeWaylandScanner(
+        b,
+        "client-header",
+        protocol_xml_path,
+        output_basename,
+    );
+}
+
+fn generateClientSource(
+    b: *std.Build,
+    protocol_xml_path: []const u8,
+    output_basename: []const u8,
+) std.Build.LazyPath {
+    return invokeWaylandScanner(
+        b,
+        "private-code",
+        protocol_xml_path,
+        output_basename,
+    );
 }
 
 fn invokeWaylandScanner(
